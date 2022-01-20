7178b33bbcce44a301486286e89f0d92773e74e8
mikhail19283 <mikhail19283@gmail.com>
2022-01-20 07:00:11 +0500

    Initial Commit

---
+++ RYGlights/RYGlightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RYGlightsApp.swift
+//  RYGlights
+//
+//  Created by Mikhail Kamynin on 20.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct RYGlightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
